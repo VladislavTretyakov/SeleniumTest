49e8726dd46b372750d122663d9f0441a660b3cc
Vladislav Tretyakov <ididnothing86@gmail.com>
2022-12-11 23:41:52 +0600

    correctionBE

--- src/test/java/ru/netology/web/TestOrder.java
+++ src/test/java/ru/netology/web/TestOrder.java
@@ -1,20 +1,23 @@
 package ru.netology.web;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.*;
 
 import static com.codeborne.selenide.Selenide.*;
 
 public class TestOrder {
-//        private WebDriver driver;
-//
-//        @BeforeEach
-//        void setUp() {driver = new ChromeDriver();}
+
+        @BeforeEach
+        public void eachTest() {
+            Configuration.holdBrowserOpen = true;
+            Configuration.browserSize = "800x700";
+            open("http://localhost:9999/");
+        }
 
     @Test
     void validationTestV1() {
-        open("http://localhost:9999/");
         SelenideElement form = $("div[id=root]");
         form.$("span[data-test-id=name] input").setValue("Третьяков Владислав");
         form.$("span[data-test-id=phone] input").setValue("+79994445566");
@@ -25,7 +28,6 @@ public class TestOrder {
 
     @Test
     void validationHyphenatedTest() {
-        open("http://localhost:9999/");
         SelenideElement form = $("div[id=root]");
         form.$("span[data-test-id=name] input").setValue("Третьяков-Ощепков Владислав");
         form.$("span[data-test-id=phone] input").setValue("+79994445566");
@@ -36,7 +38,6 @@ public class TestOrder {
 
     @Test
     void validationLatinTest() {
-        open("http://localhost:9999/");
         SelenideElement form = $("div[id=root]");
         form.$("span[data-test-id=name] input").setValue("Tretyakov Vladislav");
         form.$("span[data-test-id=phone] input").setValue("+79994445566");
@@ -47,7 +48,6 @@ public class TestOrder {
 
     @Test
     void incompleteNumberTest() {
-        open("http://localhost:9999/");
         SelenideElement form = $("div[id=root]");
         form.$("span[data-test-id=name] input").setValue("Третьяков Владислав");
         form.$("span[data-test-id=phone] input").setValue("+7999444566");
