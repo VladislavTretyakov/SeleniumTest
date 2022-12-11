03df54589fb5402e261565c97d0ca618195c9e24
Vladislav Tretyakov <ididnothing86@gmail.com>
2022-12-11 23:22:51 +0600

    correction

--- src/test/java/ru/netology/web/TestOrder.java
+++ src/test/java/ru/netology/web/TestOrder.java
@@ -1,111 +1,59 @@
 package ru.netology.web;
 
-import com.codeborne.selenide.CollectionCondition;
 import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.*;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 import static com.codeborne.selenide.Selenide.*;
 
 public class TestOrder {
-//    private WebDriver driver;
+//        private WebDriver driver;
 //
-//    @BeforeAll
-//    static void setUpAll() {
-//        System.setProperty("webdriver.chrome.driver", "./driver/win/chromedriver.exe");
-//    }
-//
-//    @BeforeEach
-//    void setUp() {driver = new ChromeDriver();}
-//
-//    @AfterEach
-//    void tearDown() {
-//        driver.quit();
-//        driver = null;
-//    }
-
-//    @Test
-//    void validationTestV1() throws InterruptedException {
-//        driver.get("http://localhost:9999/");
-//        driver.findElement(By.cssSelector("span[data-test-id=name] input")).sendKeys("Третьяков Владислав");
-//        driver.findElement(By.cssSelector("span[data-test-id=phone] input")).sendKeys("+79994445566");
-//        driver.findElement(By.cssSelector("label[data-test-id=agreement]")).click();
-//        driver.findElement(By.className("button")).click();
-//        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
-//        String actual = driver.findElement(By.cssSelector("p[data-test-id=order-success]")).getText().trim();
-//        Assertions.assertEquals(expected, actual);
-//    }
+//        @BeforeEach
+//        void setUp() {driver = new ChromeDriver();}
 
     @Test
-    void validationTestV1() throws InterruptedException {
+    void validationTestV1() {
         open("http://localhost:9999/");
         SelenideElement form = $("div[id=root]");
-        form.$(By.cssSelector("span[data-test-id=name] input")).setValue("Третьяков Владислав");
-        form.$(By.cssSelector("span[data-test-id=phone] input")).setValue("+79994445566");
-        form.$(By.cssSelector("label[data-test-id=agreement]")).click();
-        form.$(By.className("button")).click();
+        form.$("span[data-test-id=name] input").setValue("Третьяков Владислав");
+        form.$("span[data-test-id=phone] input").setValue("+79994445566");
+        form.$("label[data-test-id=agreement]").click();
+        form.$("button").click();
         $("p[data-test-id=order-success]").shouldHave(Condition.exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
-//    @Test
-//    void validationHyphenatedTest() throws InterruptedException {
-//        driver.get("http://localhost:9999/");
-//        driver.findElement(By.cssSelector("span[data-test-id=name] input")).sendKeys("Третьяков-Ощепков Владислав");
-//        driver.findElement(By.cssSelector("span[data-test-id=phone] input")).sendKeys("+79994445566");
-//        driver.findElement(By.cssSelector("label[data-test-id=agreement]")).click();
-//        driver.findElement(By.className("button")).click();
-//        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
-//        String actual = driver.findElement(By.cssSelector("p[data-test-id=order-success]")).getText().trim();
-//        Assertions.assertEquals(expected, actual);
-//    }
 
     @Test
-    void validationHyphenatedTest() throws InterruptedException {
+    void validationHyphenatedTest() {
         open("http://localhost:9999/");
         SelenideElement form = $("div[id=root]");
-        form.$(By.cssSelector("span[data-test-id=name] input")).setValue("Третьяков-Ощепков Владислав");
-        form.$(By.cssSelector("span[data-test-id=phone] input")).setValue("+79994445566");
-        form.$(By.cssSelector("label[data-test-id=agreement]")).click();
-        form.$(By.className("button")).click();
+        form.$("span[data-test-id=name] input").setValue("Третьяков-Ощепков Владислав");
+        form.$("span[data-test-id=phone] input").setValue("+79994445566");
+        form.$("label[data-test-id=agreement]").click();
+        form.$("button").click();
         $("p[data-test-id=order-success]").shouldHave(Condition.exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
-//
-//    @Test
-//    void validationLatinTest() throws InterruptedException {
-//        driver.get("http://localhost:9999/");
-//        driver.findElement(By.cssSelector("span[data-test-id=name] input")).sendKeys("Tretyakov Vladislav");
-//        driver.findElement(By.cssSelector("span[data-test-id=phone] input")).sendKeys("+79994445566");
-//        driver.findElement(By.cssSelector("label[data-test-id=agreement]")).click();
-//        driver.findElement(By.className("button")).click();
-//        String expected = "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.";
-//        String actual = driver.findElement(By.className("input__sub")).getText().trim();
-//        Assertions.assertEquals(expected, actual);
-//    }
 
     @Test
-    void validationLatinTest() throws InterruptedException {
+    void validationLatinTest() {
         open("http://localhost:9999/");
         SelenideElement form = $("div[id=root]");
-        form.$(By.cssSelector("span[data-test-id=name] input")).setValue("Tretyakov Vladislav");
-        form.$(By.cssSelector("span[data-test-id=phone] input")).setValue("+79994445566");
-        form.$(By.cssSelector("label[data-test-id=agreement]")).click();
-        form.$(By.className("button")).click();
-        $(".input__sub").shouldHave(Condition.exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+        form.$("span[data-test-id=name] input").setValue("Tretyakov Vladislav");
+        form.$("span[data-test-id=phone] input").setValue("+79994445566");
+        form.$("label[data-test-id=agreement]").click();
+        form.$("button").click();
+        $("[data-test-id='name'].input_invalid .input__sub").shouldHave(Condition.exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+
+    @Test
+    void incompleteNumberTest() {
+        open("http://localhost:9999/");
+        SelenideElement form = $("div[id=root]");
+        form.$("span[data-test-id=name] input").setValue("Третьяков Владислав");
+        form.$("span[data-test-id=phone] input").setValue("+7999444566");
+        form.$("label[data-test-id=agreement]").click();
+        form.$("button").click();
+        $("[data-test-id='phone'].input_invalid .input__sub").shouldHave(Condition.exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
     }
 }
-
-//    @Test
-//    void wrongNumberTest() throws InterruptedException {
-//        driver.get("http://localhost:9999/");
-//        driver.findElement(By.cssSelector("span[data-test-id=name] input")).sendKeys("Третьяков Владислав");
-//        driver.findElement(By.cssSelector("span[data-test-id=phone] input")).sendKeys("+7999444556");
-//        driver.findElement(By.cssSelector("label[data-test-id=agreement]")).click();
-//        driver.findElement(By.className("button")).click();
-//        String expected = "Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.";
-//        String actual = driver.findElement(By.className("input__sub")).getText().trim();
-//        Assertions.assertEquals(expected, actual);
-//    }
 
